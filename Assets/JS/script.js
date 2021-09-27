060ed42c97c51b37afd2420cdefe7d6598d75bec
Leci1259 <lreid31@gatech.edu>
2021-09-27 16:27:42 -0400

    set up function to grab city information; function for the submit button; function for the previous buttons to reinitalize

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,11 +1,70 @@
+//Grab elements off the page
 var todayWeatherInfo = $(".today")
 var laterWeatherInfo = $(".week")
 var cityInput = $("input")
+var buttonHolder= $("#button-holder")
+var submitButton= $("submit")
+var sideList = $("side-buttons")
 
+//api key
+var ApiKey='949cfd5d625b3cd45f6ad2527b38c843';
 
+//variables needed
+var city= $(".city").val().trim();
 
-var appendButton = function (cityName) {
-    
+//append city buttons to side
+var appendButton = function (city) {
 
+    //create button elements
+    var cityButton = document.createElement("button");
+
+    //add class and text content
+    cityButton.textContent= city;
+    cityButton.classList="side-buttons"
+
+    //append button
+    buttonHolder.appendChild(cityButton);
+}
+
+//get city's info
+var myCityInfo = function (city) {
+     
+        //construct proper url
+        var cityURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + ApiKey;
+        //fetch data
+        fetch(cityURL)
+        .then(function (response) {
+            return response.json();
+        })
+        //pull needed info from fetched data
+        .then(function (data) {
+            //send data through another function to get weather
+            myWeatherInfo(data[0].lat,data[0].long,city)
+        })   
+};
+
+var myWeatherInfo = function (lat,long,city) {
 
 }
+
+//on submit button click
+submitButton.addEventlistener('click', function(event) {
+    //keeps text up to read
+    event.preventDefault();
+    //grabs city input
+    var city= $(".city").val().trim();
+
+     //if not empty
+    if (city) {
+        //run through function
+        myCityInfo(city)
+    }
+    else {
+        alert("Error! City not entered correctly. Try again.")
+    }
+
+    //clears input
+    $(".city").text('');
+})
+
+//on side button click
