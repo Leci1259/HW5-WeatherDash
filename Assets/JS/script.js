b96dd3659c9f64a8907aa24a08e1295481297e24
Leci1259 <lreid31@gatech.edu>
2021-09-27 17:31:45 -0400

    added second function to grab today's weather and display to page; to change uvi index background; added metrics to the html

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,11 +3,11 @@ var todayWeatherInfo = $(".today")
 var laterWeatherInfo = $(".week")
 var cityInput = $("input")
 var buttonHolder= $("#button-holder")
-var submitButton= $("submit")
-var sideList = $("side-buttons")
+var submitButton= $("#submit")
+
 
 //api key
-var ApiKey='949cfd5d625b3cd45f6ad2527b38c843';
+var ApiKey='6b48a9a445f2d6135a213e01e7d1d9cc';
 
 //variables needed
 var city= $(".city").val().trim();
@@ -30,7 +30,7 @@ var appendButton = function (city) {
 var myCityInfo = function (city) {
      
         //construct proper url
-        var cityURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + ApiKey;
+        var cityURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "appid=" + ApiKey;
         //fetch data
         fetch(cityURL)
         .then(function (response) {
@@ -38,17 +38,59 @@ var myCityInfo = function (city) {
         })
         //pull needed info from fetched data
         .then(function (data) {
-            //send data through another function to get weather
-            myWeatherInfo(data[0].lat,data[0].long,city)
-        })   
+            console.log(data)
+            
+           //send data through another function to get weather 
+            myWeatherInfo(data.coord.lat,data.coord.long,city);
+        }); 
 };
 
-var myWeatherInfo = function (lat,long,city) {
+var myWeatherInfo = function (lat,long,name) {
+    
+    //construct proper url
+    var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&appid=" + ApiKey;
+    //fetch data
+    fetch(weatherURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function (data) {
+
+     //today temperature
+    todayWeatherInfo[0].text(data[0].current.temp)
+
+    //today wind
+    todayWeatherInfo[1].text(data.current.wind_speed)
+
+    // today humidity
+    todayWeatherInfo[2].text(data.current.humidity)  
+    
+    //today uvi
+    todayWeatherInfo[3].text(data.current.uvi) 
+    //uvi color change
+    if (data.current.uvi < 3) {
+        todayWeatherInfo[3].css("background-color", "green")   
+    }
+    else if (data.current.uvi < 6) {
+        todayWeatherInfo[3].css("background-color", "yellow") 
+    }
+    else if (data.current.uvi < 8) {
+        todayWeatherInfo[3].css("background-color", "orange") 
+    }
+    else if (data.current.uvi < 11) {
+        todayWeatherInfo[3].css("background-color", "red") 
+    }
+    else {
+        todayWeatherInfo[3].css("background-color", "purple") 
+    }
+
+})
+
 
 }
 
 //on submit button click
-submitButton.addEventlistener('click', function(event) {
+submitButton.on('click', function(event) {
     //keeps text up to read
     event.preventDefault();
     //grabs city input
@@ -65,6 +107,17 @@ submitButton.addEventlistener('click', function(event) {
 
     //clears input
     $(".city").text('');
-})
+});
 
 //on side button click
+buttonHolder.on('click', function(event) {
+    //identify which button
+    var button = event.target
+
+    //get the city from that button
+    city = button.text();
+
+    //run city through weather data process again
+    myCityInfo(city)
+
+});
