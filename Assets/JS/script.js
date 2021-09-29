dab48ee4b526687d88fe231aa4ff951eb7db2c21
Leci1259 <lreid31@gatech.edu>
2021-09-28 21:07:06 -0400

    reorganized js file; fixed typos; got icons included

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -4,97 +4,109 @@ var laterWeatherInfo = $(".week")
 var cityInput = $("input")
 var buttonHolder= $("#button-holder")
 var submitButton= $("#submit")
-var city= $(".city").val().trim();
 var laterWeatherDates= $(".weekHead")
-
+var storedCity;
+var city= $(".city").val().trim();
+var images = $("img")
 //api key
-var ApiKey="949cfd5d625b3cd45f6ad2527b38c843";
+var ApiKey="6b48a9a445f2d6135a213e01e7d1d9cc";
+
 
 //get city's info
 var myCityInfo = function (city) {
-     
+
         //construct proper url
-        var cityURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "appid=" + ApiKey;
+        var cityURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + ApiKey;
         //fetch data
         fetch(cityURL)
         .then(function (response) {
             return response.json();
         })
         //pull needed info from fetched data
-        .then(function (data) {
-            console.log(data)
-            
-           //send data through another function to get weather 
-            myWeatherInfo(data.coord.lat,data.coord.long,city);
+        .then(function (data) {            
+            //send data through another function to get weather 
+            myWeatherInfo(data.coord.lat,data.coord.lon,city);
         }); 
+        //append button with city name
+        appendButton(city.toUpperCase());
 };
 
 
-var myWeatherInfo = function (lat,long,name) {
+var myWeatherInfo = function (lat,lon,name) {
     
     //construct proper url
-    var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&appid=" + ApiKey;
+    var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + ApiKey;
     //fetch data
     fetch(weatherURL)
     .then(function(response) {
         return response.json();
     })
     .then(function (data) {
-
-        //display name and date 
-        $("#cityName").text(name + moment().format("MM/DD/YYYY"))
-
-        //today temperature
-        todayWeatherInfo[0].text(data.daily[0].temp)
-
-        //today wind
-        todayWeatherInfo[1].text(data.daily[0].wind_speed)
-
-        // today humidity
-        todayWeatherInfo[2].text(data.daily[0].humidity)  
-        
-        //today uvi
-        todayWeatherInfo[3].text(data.daily[0].uvi) 
-
-        //uvi color change
-        if (data.current.uvi < 3) {
-            todayWeatherInfo[3].css("background-color", "green")   
-        }
-        else if (data.current.uvi < 6) {
-            todayWeatherInfo[3].css("background-color", "yellow") 
-        }
-        else if (data.current.uvi < 8) {
-            todayWeatherInfo[3].css("background-color", "orange") 
-        }
-        else if (data.current.uvi < 11) {
-            todayWeatherInfo[3].css("background-color", "red") 
-        }
-        else {
-            todayWeatherInfo[3].css("background-color", "purple") 
-        }
+        console.log(data)   
+        displayData(data)
+    })
+}
 
 
-        //display for next five days
-        var x =1;
-        var y = 0;
-        for (var i = 0; i<laterWeatherInfo.length;i++) {
+var displayData = function (data) {
+    //display name and date 
+    $("#cityName").text(name.toUpperCase() +" " + moment().format("MM/DD/YYYY"))
 
-            //title
-            laterWeatherDates[y].text(moment().add(x,'d').format("MM/DD/YYYY") )
+    //display icon
+    $(images[0]).attr("src","https://openweathermap.org/img/w/" + data.daily[0].weather[0].icon + ".png")
 
-            //temperature
-            laterWeatherInfo[i].text(data.daily[x].temp)
+    //today temperature
+    $(todayWeatherInfo[0]).text(data.daily[0].temp.day)
 
-            //wind
-            laterWeatherInfo[++i].text(data.daily[x].wind_speed)
+    //today wind
+    $(todayWeatherInfo[1]).text(data.daily[0].wind_speed)
 
-            // humidity
-            laterWeatherInfo[++i].text(data.daily[x].humidity) 
+    // today humidity
+    $(todayWeatherInfo[2]).text(data.daily[0].humidity)  
 
-            x++;
-        }
+    //today uvi
+    $(todayWeatherInfo[3]).text(data.daily[0].uvi) 
 
-})
+    //uvi color change
+    if (data.current.uvi < 3) {
+        $(todayWeatherInfo[3]).css("background-color", "green")   
+    }
+    else if (data.current.uvi < 6) {
+        $(todayWeatherInfo[3]).css("background-color", "yellow") 
+    }
+    else if (data.current.uvi < 8) {
+        $(todayWeatherInfo[3]).css("background-color", "orange") 
+    }
+    else if (data.current.uvi < 11) {
+        $(todayWeatherInfo[3]).css("background-color", "red") 
+    }
+    else {
+        $(todayWeatherInfo[3]).css("background-color", "purple") 
+    }
+
+
+    //display for next five days
+    var x =1;
+    var y = 0;
+    for (var i = 0; i<laterWeatherInfo.length;i++) {
+
+    //title
+    $(laterWeatherDates[y]).text(moment().add(x,'d').format("MM/DD/YYYY"))
+
+    //icon
+    $(images[x]).attr("src","https://openweathermap.org/img/w/" + data.daily[x].weather[0].icon + ".png")
+
+    //temperature
+    $(laterWeatherInfo[i]).text(data.daily[x].temp.day)
+    //wind
+    $(laterWeatherInfo[++i]).text(data.daily[x].wind_speed)
+
+    // humidity
+    $(laterWeatherInfo[++i]).text(data.daily[x].humidity) 
+
+    x++;
+    y++;
+    } 
 }
 
 //append city buttons to side
@@ -109,6 +121,10 @@ var appendButton = function (city) {
 
     //append button
     buttonHolder.append(cityButton);
+
+    //change value of storedCity and save it to local storage
+    storedCity=city;
+    localStorage.setItem('storedCity',storedCity);
 }
 
 //on submit button click
@@ -146,3 +162,18 @@ buttonHolder.on('click', function(event) {
     myCityInfo(city)
 
 });
+
+//run function and show last searched info
+function init () {
+    //Grab stored city list from local storage
+    var lastCity = localStorage.getItem("storedCity")
+    
+    if (lastCity) {
+        city=lastCity;
+       //Run through function 
+        myCityInfo(city)
+    }
+    
+    
+}
+init();
